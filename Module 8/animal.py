bb6f84278d7b842dba52c133924cb7189ae66d9b
DiarBerisha <154000780+DiarBerisha@users.noreply.github.com>
2026-03-07 12:44:59 +0100

    module9

--- Module 8/animal.py
+++ Module 8/animal.py
@@ -1,11 +1,11 @@
 class Animal:
-    def __init__(self, typeOfAnimal, race, ngjyra):
-        self.typeOfAnimal = typeOfAnimal
+    def __init__(self, typeofanimal, race, ngjyra):
+        self.typeofanimal = typeofanimal
         self.race = race
         self.ngjyra = ngjyra
 
     def greet(self):
-        print("The type of animal is:", self.typeOfAnimal, "The race is:", self.race, "The color is:", self.ngjyra)
+        print("The type of animal is:", self.typeofanimal, "The race is:", self.race, "The color is:", self.ngjyra)
 
 dog = Animal('dog', "husky", "white")
 cat = Animal('cat', "keqe", "white")
